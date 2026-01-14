084bb8588108dc71cb30416174d9f5b7dfbb4344
Kaique Borges <rique.kaique@gmail.com>
2026-01-13 21:18:35 -0300

    feat: :sparkles: set up a minimal express server

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,22 @@
 import { sayHello } from "@prisma-finance/core";
+import express, { Response } from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3333;
 
 export function foo() {
   return "bar";
 }
 
-console.log("Hello from API");
-console.log(sayHello());
+app.get("/", (_, res: Response) => {
+  res.send(sayHello());
+});
+
+try {
+  app.listen(port, () => {
+    console.log(`Server listening at port ${port}...`);
+  });
+} catch (error) {
+  console.error(`Error when starting server: ${error}`);
+}
