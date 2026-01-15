b59d85215ee840a9f51e484f5c30b5c910ec1693
Kaique Borges <rique.kaique@gmail.com>
2026-01-15 11:21:51 -0300

    test: fix broken sample test for Next.js using mock fetch

--- apps/web/src/app/page.test.tsx
+++ apps/web/src/app/page.test.tsx
@@ -2,7 +2,14 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Page from "./page";
 
-test("Page", () => {
-  render(<Page />);
-  expect(screen.getByRole("heading", { level: 1 })).toBeDefined();
+test("Page has a heading", async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({ message: "Hello, world!" }));
+
+  const page = await Page();
+
+  render(page);
+
+  expect(screen.getByRole("heading", { level: 1 }).textContent).toContain(
+    "Hello",
+  );
 });
